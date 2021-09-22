24382d40aaca774ed92a168c14d771ec72736a9e
x <x@to.wtf>
2021-09-22 16:46:44 +0300

    :fire:

--- dapp/constants.js
+++ dapp/constants.js
@@ -1,11 +1,11 @@
 //mainnet
-// export const CONTRACT_ADDR = '0x80a4B80C653112B789517eb28aC111519b608b19'
-// export const NETWORK_ID = 1
-// export const RPC_PROVIDER =
-//   'https://eth-mainnet.alchemyapi.io/v2/oVryicy8q6o6iFj2sv_DZHR8KIUNDVYu'
+export const CONTRACT_ADDR = '0x093D74Cc028f888cC354735c624E467B942883FF'
+export const NETWORK_ID = 1
+export const RPC_PROVIDER =
+  'https://eth-mainnet.alchemyapi.io/v2/jhdxp08tZa-3h9KVpwlWyi6np2zmjXe9'
 
 //rinkeby
-export const CONTRACT_ADDR = '0xd37Ab3260324FD91F5A24d566A691058fd976dEb'
-export const NETWORK_ID = 4
-export const RPC_PROVIDER =
-  'https://eth-rinkeby.alchemyapi.io/v2/uby1uWDgjRI2VpHXA-VDQQUzlY0k5mwA'
+// export const CONTRACT_ADDR = '0xd37Ab3260324FD91F5A24d566A691058fd976dEb'
+// export const NETWORK_ID = 4
+// export const RPC_PROVIDER =
+//   'https://eth-rinkeby.alchemyapi.io/v2/uby1uWDgjRI2VpHXA-VDQQUzlY0k5mwA'
